e8925fb45748ecc1c0aec50c169ed4017eeee2ce
Eduardo Xerxenevsky <eduardoxer@gmail.com>
2023-09-27 21:07:32 -0300

    Atualização Banco de dados

--- PWMA_ENTREGAS/PWMA-Semana#07/atividade 7/js/bd.js
+++ PWMA_ENTREGAS/PWMA-Semana#07/atividade 7/js/bd.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Simulação de banco de dados (array)
     const database = [
-        { id: 1, tipoLogin: 'Professor', situacao: 'Online' },
-        { id: 2, tipoLogin: 'Aluno', situacao: 'Offline' },
-        { id: 3, tipoLogin: 'Professor', situacao: 'Online' },
-        { id: 4, tipoLogin: 'Aluno', situacao: 'Online' },
-        { id: 5, tipoLogin: 'Professor', situacao: 'Offline' },
+        { id: 1, tipoLogin: 'Professor', situacao: 'Online', login: 'Adalberto123' , senha: 'coxinha123'},
+        { id: 2, tipoLogin: 'Aluno', situacao: 'Offline' , login: 'C4rl1nh05' , senha: 'P@$$w0rd'},
+        { id: 3, tipoLogin: 'Professor', situacao: 'Online' , login: 'MarcelinhoCarioca' , senha: 'vasco123'},
+        { id: 4, tipoLogin: 'Aluno', situacao: 'Online' , login: 'LuisAlbertoSuarez' , senha: 'Am0M3ss1'},
+        { id: 5, tipoLogin: 'Professor', situacao: 'Offline' , login: 'G4biG0l' , senha: 'V4iC0r1nth1@ns'},
     ];
 
     // Função para exibir os dados na tabela
@@ -18,7 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
             row.innerHTML = `
                 <td>${user.id}</td>
                 <td>${user.tipoLogin}</td>
-                <td>${user.situacao}</td>
+                <td class="${user.situacao.toLowerCase()}">${user.situacao}</td>
+                <td>${user.login}</td>
+                <td>${user.senha}</td>
             `;
             userList.appendChild(row);
         });
